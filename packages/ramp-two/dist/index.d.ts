8b39621060af0f0e44a40aa6c0c0a2c8edf7284f
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2019-08-26 08:30:01 -0400

    chore: add dist folders to gitignore

--- packages/ramp-two/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare function doScience(): {
-    line: string;
-    emoji: string[];
-};
-export { doScience };
-export * from './random-emoji';
--- packages/ramp-two/dist/index.js
+++
@@ -1,5 +0,0 @@
-function doScience() {
-    return { line: 'Science is awesome... ', emoji: ['👩‍🔬', '🧨', '🚀'] };
-}
-export { doScience };
-export * from './random-emoji';
